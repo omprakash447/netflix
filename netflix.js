c31903d2b7322b765b4ac263799372f2420475bb
om prakash lenka <150693046+omprakash447@users.noreply.github.com>
2024-03-31 03:45:32 +0530

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,197 @@
+let movies = [{
+        name: "RRR",
+        poster: "https://i2.wp.com/www.newsbugz.com/wp-content/uploads/2021/05/jr-ntr-RRR-movie-scaled.jpg?resize=1075%2C1536&ssl=1",
+        rating: 8.7
+    },
+    {
+        name: "SpiderMan",
+        poster: "https://th.bing.com/th/id/OIP.G-2Q6Qo4uqjpTCd07OXTYgHaK-?rs=1&pid=ImgDetMain",
+        rating: 8.9
+    },
+    {
+        name: "Adipurush",
+        poster: "https://th.bing.com/th/id/OIP.DKUy8Pvg1p5lsm36t1UadQHaKD?rs=1&pid=ImgDetMain",
+        rating: 7.8
+    },
+    {
+        name: "Bhola",
+        poster: "https://assets.gadgets360cdn.com/pricee/assets/product/202301/Bholaa_1674473942.jpg",
+        rating: 6.9
+    },
+    {
+        name: "Jawan",
+        poster: "https://th.bing.com/th/id/OIP.AkdK8UnwIEMU8VusqfVUIwHaLq?rs=1&pid=ImgDetMain",
+        rating: 9.0
+    },
+    {
+        name: "War",
+        poster: "https://th.bing.com/th/id/OIP.jbmDB4MGpeZb_2Oh0Zt8WAHaKG?rs=1&pid=ImgDetMain",
+        rating: 8.7
+    },
+    {
+        name: "Titanic",
+        poster: "https://inspgr.id/app/uploads/2021/01/illustration-alexey-kot-01.jpg",
+        rating: 9.5
+    },
+    {
+        name: "Maaster",
+        poster: "https://th.bing.com/th/id/OIP.d5o3l3_2V9GuGv-f9ygsTQHaJ4?rs=1&pid=ImgDetMain",
+        rating: 8.2
+    },
+    {
+        name: "Bigil",
+        poster: "https://i0.wp.com/www.mrdhukkad.com/wp-content/uploads/2019/06/7-2.jpg",
+        rating: 7.9
+    },
+    {
+        name: "Valimai",
+        poster: "https://th.bing.com/th/id/OIP.pkBAQWfAC8iYCYD61PqoTgAAAA?rs=1&pid=ImgDetMain",
+        rating: 5.6
+    },
+    {
+        name: "Abduction",
+        poster: "https://img.izismile.com/img/img5/20121207/640/modern_movie_poster_trends_640_high_07.jpg",
+        rating: 9.5
+    },
+    {
+        name: "Survival Night",
+        poster: "https://th.bing.com/th/id/OIP.1oChlLDscMn5Rj8BAf46QwHaH8?rs=1&pid=ImgDetMain",
+        rating: 7.9
+    },
+    {
+        name: "Beast",
+        poster: "https://www.tamilnow.com/movies/gallery/beast/new-still-thalapathy-vijay-beast-161.jpg",
+        rating: 8.5
+    },
+    {
+        name: "DJ",
+        poster: "https://th.bing.com/th/id/OIP.F8BPrTaggGPq1cqLDwaaLwDSEp?rs=1&pid=ImgDetMain",
+        rating: 6.9
+    },
+    {
+        name: "12th fail",
+        poster: "https://images.ottplay.com/images/12th-fail-639.jpg",
+        rating: 9.0
+    },
+    {
+        name: "Legend Hanuman",
+        poster: "https://i.ytimg.com/vi/Af49v6rUXmQ/maxresdefault.jpg",
+        rating: 7.8
+    },
+    {
+        name: "Jumanji 3",
+        poster: "https://mediavideo.blastingnews.com/p/4/2019/04/12/7c57b3d1-7aba-4686-bb1c-239fffd666fc.jpg",
+        rating: 7.9
+    },
+    {
+        name: "Harry porter",
+        poster: "https://th.bing.com/th/id/R.d18f3abaeae7876693207c2927d5e7f2?rik=WLo7ILVa4S30Gw&riu=http%3a%2f%2fimages5.fanpop.com%2fimage%2fphotos%2f24800000%2fHarry-Potter-harry-potter-24869135-1280-1024.jpg&ehk=s5gWWBac7tc1%2fIIKjnrEutbkIBzAsJ87hLI3z%2ffIvP0%3d&risl=&pid=ImgRaw&r=0",
+        rating: 9.5
+    },
+    {
+        name: "Spirited Away",
+        poster: "https://www.scrolldroll.com/wp-content/uploads/2021/07/spirited-away-best-japanese-anime-movies-1036x1536.jpg",
+        rating: 8.1
+    },
+    {
+        name: "Fighter",
+        poster: "https://nettv4u.com/fileman/Uploads2/z_fighter.jpg",
+        rating: 8.4
+    },
+    {
+        name: "Tanhaji",
+        poster: "https://boldoutline.in/wp-content/uploads/2019/10/Web-Cover-99.jpg",
+        rating: 6.1
+    },
+    {
+        name: "Half Girlfriend",
+        poster: "https://th.bing.com/th/id/OIP.73Y-e7iYq8GgTNiUesG1bQAAAA?rs=1&pid=ImgDetMain",
+        rating: 4.9
+    },
+    {
+        name: "Heartless",
+        poster: "https://th.bing.com/th/id/R.be74797522fc767e1fd2b5af68a70646?rik=PPTKiEjsGvEe8Q&riu=http%3a%2f%2fwww.bollywoodlife.com%2fwp-content%2fuploads%2f2014%2f02%2fPoster-1.jpg&ehk=kkl5aCLU%2fxGkYoapYbjB30V%2fmfIwvTvoL%2fuj2Mp9g3Y%3d&risl=&pid=ImgRaw&r=0",
+        rating: 6.9
+    },
+    {
+        name: "lagan",
+        poster: "https://static-koimoi.akamaized.net/wp-content/new-galleries/2021/06/reunion-special-chale-chalo-lagaan-once-upon-to-mark-20-years-of-film-001.jpg",
+        rating: 8.1
+    },
+    {
+        name: "Wakanda Forever",
+        poster: "https://www.cinetrafic.fr/images/affiches/original/aff_6466020181027150448.png",
+        rating: 9.9
+    },
+    {
+        name: "Strom",
+        poster: "https://i.ytimg.com/vi/z6ikcmqGcGg/maxresdefault.jpg",
+        rating: 8.9
+    },
+    {
+        name: "Avtar",
+        poster: "https://th.bing.com/th/id/OIP.takhz3-GKOxbptYGZ4ORFQHaEo?rs=1&pid=ImgDetMain",
+        rating: 9.9
+    },
+    {
+        name: "Fast and furious",
+        poster: "https://wallpapercave.com/wp/ok4EoNj.jpg",
+        rating: 8.6
+    },
+    {
+        name: "Ganpat",
+        poster: "https://i0.wp.com/www.newsbugz.com/wp-content/uploads/2021/08/ganapath-movie.jpg?resize=1536%2C877&ssl=1",
+        rating: 6.6
+    },
+    {
+        name: "Shiddat",
+        poster: "https://assets.gadgets360cdn.com/pricee/assets/product/202209/Siddhat_1663043806.jpg",
+        rating: 7.6
+    },
+]
+
+function displaymoveis(data) {
+
+    document.getElementById("movies").innerHTML = "";
+
+    let divstring = ``;
+
+
+    for (let i = 0; i < data.length; i++) {
+        divstring = divstring + `
+    <div class="movei">
+    <div class="overlay">
+        <div class="vedio">
+  
+        </div>
+        <div class="details">
+            <h1>${data[i].name}</h1>
+            <h2>IMBD :${data[i].rating}</h2>
+            <p>Ram charan , Junior ntr , Alliabhatt</p>
+        </div>
+    </div>
+    <img id="imgs" class="poster" src="${data[i].poster}" alt="">
+  </div>`;
+    }
+
+    document.getElementById("movies").innerHTML = divstring;
+    // console.log(divstring)
+
+}
+displaymoveis(movies);
+
+
+
+
+
+
+function searchmovei() {
+    let moviename = document.getElementById("search").value;
+    if (moviename !== '') {
+        let result = movies.filter(function(movei) {
+            return movei.name.toLowerCase().includes(moviename.toLowerCase());
+        })
+        displaymoveis(result);
+    }
+}
+// searchmovei();
